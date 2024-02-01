a854489c8f87de600f09dad9f20cb8b9fb77e554
Marc Baqué <marcbaque96@gmail.com>
2024-02-01 13:02:24 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzYyMA/spectral.js?branch_slug=master&token=f217e482-ee27-441e-b289-544ac52c875a";
+export default { extends: ruleset };
